9f88b3971423a0c94fcd0972b720d36ac43e77e2
William Ulff <william.ulff@koncepta.se>
2026-02-09 14:12:39 +0100

    uuuh

--- components/Name.tsx
+++ components/Name.tsx
@@ -4,10 +4,10 @@ export default function Name() {
   return (
     <>
       <div id="name-container" className="flex items-center justify-center">
-        <span id="i-blink" className="text-7xl text-white">
+        <span id="i-blink" className="text-7xl text-(--off-white)">
           |
         </span>
-        <span className="text-7xl text-white mt-3"> William Ulff</span>
+        <span className="text-7xl text-(--off-white) mt-3"> William Ulff</span>
       </div>
     </>
   );
